892d64cf44d83ec55a869675bea678fb132d57af
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2023-03-02 14:35:09 +0000

    created a component for regular interval loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
 import { useEffect } from "react"
 
-import DialogContentText from "@mui/material/DialogContentText"
-
 import CustomDialog from "./components/CustomDialog"
+import Loader from "./components/Loader"
 
 import useFfmpeg from "./ffmpeg/useFfmpeg"
 
-import { CustomDialogProps } from "./components/CustomDialog/types"
-
-type CustomProps = Omit<CustomDialogProps, "children">
 
 function App() {
 	const { ready, load } = useFfmpeg()
 
-	const props: CustomProps = {
-		title: "Testing dialog button trigger",
-		trigger: {
-			type: "Button",
-			color: "primary",
-			size: "small",
-			variant: "contained",
-			tooltipPlacement: "top",
-			tooltipTitle: "Testing button trigger",
-		},
-	}
-
 	useEffect(() => {
 		if (process.env.NODE_ENV !== "test") {
-			// load()
+			load()
 		}
 	}, [])
 
@@ -35,12 +19,8 @@ function App() {
 		<>
 			<h1>Hello World!</h1>
 			{ready ? "ready" : "loading..."}
-			<CustomDialog {...props}>
-				<DialogContentText id="alert-dialog-slide-description">
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic deserunt assumenda
-					quisquam reiciendis corrupti vitae numquam asperiores similique mollitia
-					aspernatur libero nulla blanditiis nisi esse quia, velit nam, doloremque magni!
-				</DialogContentText>
+			<CustomDialog title="test" openOnMount maxWidth='sm' fullWidth hideCloseBtn>
+				<Loader status={ready ? 100 : undefined} />
 			</CustomDialog>
 		</>
 	)
---
+++ src/components/Loader/useRegularInterval.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react"
+
+interface Props {
+	growingValue?: number
+	time?: number
+	maxLimit?: number
+}
+
+const useRegularIntervals = ({ growingValue, time, maxLimit }: Props) => {
+	const intervalValue =
+		growingValue && growingValue > 0 && growingValue <= 100 ? growingValue : 10
+
+	const intervalTime = time && time > 0 ? time : 1000
+	const limit = maxLimit && maxLimit > 0 && maxLimit <= 100 ? maxLimit : 90
+
+	const [progress, setProgress] = useState(intervalValue)
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setProgress((prevProgress) => {
+				if (prevProgress < limit) {
+					return prevProgress + intervalValue
+				}
+
+				clearInterval(timer)
+
+				return prevProgress
+			})
+		}, intervalTime)
+		return () => {
+			clearInterval(timer)
+		}
+	}, [])
+
+	return { progress }
+}
+
+export default useRegularIntervals
