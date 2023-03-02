344527ec6d51984890bb1f60e0741b880f6fbcb7
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2023-03-02 08:42:43 +0000

    removed unnecesary logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
 
 import useFfmpeg from "./ffmpeg/useFfmpeg"
 
 function App() {
-	const { ffmpeg, ready, load } = useFfmpeg()
+	const { ready, load } = useFfmpeg()
 
 	useEffect(() => {
 		if (process.env.NODE_ENV !== "test") {
 			load()
 		}
-	}, [])
+	})
 
 	return (
 		<>
