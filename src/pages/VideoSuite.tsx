6c5d932a773c491d1ef0467f0616a821cb98fcc4
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2023-03-05 10:24:47 +0000

    created basic layout

--- src/pages/VideoSuite.tsx
+++ src/pages/VideoSuite.tsx
@@ -1,12 +1,26 @@
 import { FC, useState, useEffect } from "react"
 
+import { styled } from "@mui/material/styles"
+
 import Container from "@mui/material/Container"
 import Box from "@mui/material/Box"
 import CustomDialog from "../components/CustomDialog"
 import Loader from "../components/Loader"
 import SelectFile from "../components/SelectFile"
+import Paper from "@mui/material/Paper"
+import Grid from "@mui/material/Grid"
+
+import Card from "@mui/material/Card"
 
 import useFfmpeg from "../ffmpeg/useFfmpeg"
+
+const Item = styled(Paper)(({ theme }) => ({
+	backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+	...theme.typography.body2,
+	padding: theme.spacing(1),
+	textAlign: "center",
+	color: theme.palette.text.secondary,
+}))
 
 const VideoSuite: FC = () => {
 	const [openDialog, setOpenDialog] = useState(false)
@@ -24,10 +38,25 @@ const VideoSuite: FC = () => {
 
 	return (
 		<Container maxWidth="xl">
-			{/* <h1>Hello World!</h1> */}
-			<Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
+			<Box
+				sx={{
+					bgcolor: "#cfe8fc",
+					height: "100vh",
+					paddingTop: "1.5rem",
+					paddingBottom: "1.5rem",
+				}}
+			>
+				<Grid container spacing={0} sx={{ height: "100%", background: "red" }}>
+					<Grid item xs={12} sx={{ height: "50%" }}>
+						<Item>xs=8</Item>
+					</Grid>
+					<Grid item xs={12} sx={{ height: "50%", background: "green" }}>
+						<Item>xs=4</Item>
+					</Grid>
+				</Grid>
+			</Box>
 
-			<CustomDialog
+			{/* <CustomDialog
 				title={
 					ready
 						? "Please select a video file to continue"
@@ -44,7 +73,7 @@ const VideoSuite: FC = () => {
 				) : (
 					<SelectFile fileType="video" callback={() => setOpenDialog(false)} />
 				)}
-			</CustomDialog>
+			</CustomDialog> */}
 		</Container>
 	)
 }
