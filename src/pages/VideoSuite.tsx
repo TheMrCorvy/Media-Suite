f3ee12417f9bb393fca3978e3b76e6df64488b1a
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2023-03-05 11:10:58 +0000

    finished basic layout for videoSuite page

--- src/pages/VideoSuite.tsx
+++ src/pages/VideoSuite.tsx
@@ -1,26 +1,14 @@
 import { FC, useState, useEffect } from "react"
 
-import { styled } from "@mui/material/styles"
+import Grid from "@mui/material/Grid"
+import Card from "@mui/material/Card"
+import CardMedia from "@mui/material/CardMedia"
 
-import Container from "@mui/material/Container"
-import Box from "@mui/material/Box"
 import CustomDialog from "../components/CustomDialog"
 import Loader from "../components/Loader"
 import SelectFile from "../components/SelectFile"
-import Paper from "@mui/material/Paper"
-import Grid from "@mui/material/Grid"
-
-import Card from "@mui/material/Card"
 
 import useFfmpeg from "../ffmpeg/useFfmpeg"
-
-const Item = styled(Paper)(({ theme }) => ({
-	backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-	...theme.typography.body2,
-	padding: theme.spacing(1),
-	textAlign: "center",
-	color: theme.palette.text.secondary,
-}))
 
 const VideoSuite: FC = () => {
 	const [openDialog, setOpenDialog] = useState(false)
@@ -33,30 +21,46 @@ const VideoSuite: FC = () => {
 			load()
 		}
 
-		setOpenDialog(!testEnv)
+		// setOpenDialog(!testEnv)
 	}, [])
 
 	return (
-		<Container maxWidth="xl">
-			<Box
-				sx={{
-					bgcolor: "#cfe8fc",
-					height: "100vh",
-					paddingTop: "1.5rem",
-					paddingBottom: "1.5rem",
-				}}
-			>
-				<Grid container spacing={0} sx={{ height: "100%", background: "red" }}>
-					<Grid item xs={12} sx={{ height: "50%" }}>
-						<Item>xs=8</Item>
-					</Grid>
-					<Grid item xs={12} sx={{ height: "50%", background: "green" }}>
-						<Item>xs=4</Item>
+		<>
+			<Grid container spacing={0}>
+				<Grid item xs={12} sx={{ minHeight: "50vh", background: "red", padding: "1.5rem" }}>
+					<Grid container rowSpacing={2} columnSpacing={2}>
+						<Grid item xs={10} sm={6} md={4} xl={2}>
+							<Card sx={{ aspectRatio: "16 / 9" }}>
+								<CardMedia
+									sx={{ height: "100%", width: "100%" }}
+									image="./src/assets/lycoris_test.jpg"
+									title="green iguana"
+								/>
+							</Card>
+						</Grid>
 					</Grid>
 				</Grid>
-			</Box>
 
-			{/* <CustomDialog
+				<Grid
+					item
+					xs={12}
+					sx={{ minHeight: "50vh", background: "green", padding: "1.5rem" }}
+				>
+					<Grid container rowSpacing={2} columnSpacing={2}>
+						<Grid item xs={10} sm={6} md={4} xl={2}>
+							<Card sx={{ aspectRatio: "16 / 9" }}>
+								<CardMedia
+									sx={{ height: "100%", width: "100%" }}
+									image="./src/assets/lycoris_test.jpg"
+									title="green iguana"
+								/>
+							</Card>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+
+			<CustomDialog
 				title={
 					ready
 						? "Please select a video file to continue"
@@ -73,8 +77,8 @@ const VideoSuite: FC = () => {
 				) : (
 					<SelectFile fileType="video" callback={() => setOpenDialog(false)} />
 				)}
-			</CustomDialog> */}
-		</Container>
+			</CustomDialog>
+		</>
 	)
 }
 
