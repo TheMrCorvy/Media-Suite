04576acc5b34be1aa731c1e78532f8d553e74aa6
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2023-03-05 09:58:32 +0000

    removed theme customization

--- src/theme.ts
+++ src/theme.ts
@@ -2,13 +2,7 @@ import createTheme from "@mui/material/styles/createTheme"
 
 const theme = createTheme({
 	components: {
-		MuiCssBaseline: {
-			styleOverrides: `
-        h1 {
-          color: grey;
-        }
-      `,
-		},
+		MuiCssBaseline: {},
 	},
 })
 
