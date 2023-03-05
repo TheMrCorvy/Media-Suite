4b47bc14c009bb66b776459325f7aabccc89702c
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2023-03-05 12:08:35 +0000

    adding more basic info to the console

--- src/pages/VideoSuite.tsx
+++ src/pages/VideoSuite.tsx
@@ -3,6 +3,10 @@ import { FC, useState, useEffect } from "react"
 import Grid from "@mui/material/Grid"
 import Card from "@mui/material/Card"
 import CardMedia from "@mui/material/CardMedia"
+import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
+import Fab from "@mui/material/Fab"
+import PlayCircleIcon from "@mui/icons-material/PlayCircle"
 
 import CustomDialog from "../components/CustomDialog"
 import Loader from "../components/Loader"
@@ -30,6 +34,12 @@ const VideoSuite: FC = () => {
 				<Grid item xs={12} sx={{ minHeight: "50vh", background: "red", padding: "1.5rem" }}>
 					<Grid container rowSpacing={2} columnSpacing={2}>
 						<Grid item xs={10} sm={6} md={4} xl={2}>
+							<TextField
+								sx={{ width: "100%" }}
+								variant="standard"
+								defaultValue="Hello World"
+								label="Edit Name of the File"
+							/>
 							<Card sx={{ aspectRatio: "16 / 9" }}>
 								<CardMedia
 									sx={{ height: "100%", width: "100%" }}
@@ -37,6 +47,36 @@ const VideoSuite: FC = () => {
 									title="green iguana"
 								/>
 							</Card>
+						</Grid>
+						<Grid item xs>
+							<Grid container>
+								<Grid xs={12}>
+									Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit
+									tempora ex neque eum assumenda accusamus eos et! Nostrum impedit
+									velit expedita, iusto beatae excepturi ab. Reprehenderit,
+									perspiciatis dicta. A, voluptate!
+								</Grid>
+								<Grid xs={12}>
+									<Grid container>
+										<Grid item xs={8}>
+											<Button variant="contained">Load Preset</Button>
+										</Grid>
+										<div style={{ flexGrow: 1 }} />
+										<Grid item xs={4}>
+											<Grid container>
+												<Grid item xs={8}>
+													<Button variant="contained">Save Preset</Button>
+												</Grid>
+												<Grid item xs={4}>
+													<Fab color="success" aria-label="start">
+														<PlayCircleIcon />
+													</Fab>
+												</Grid>
+											</Grid>
+										</Grid>
+									</Grid>
+								</Grid>
+							</Grid>
 						</Grid>
 					</Grid>
 				</Grid>
