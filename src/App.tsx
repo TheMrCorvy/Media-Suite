cd708a8f9a34d6fde5805ee85de14a6f8ac92501
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2023-03-05 09:46:51 +0000

    moved all the logic to the videoSuite page

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,14 @@
-import { useEffect, useState } from "react"
+import CssBaseline from "@mui/material/CssBaseline"
+import ThemeProvider from "@mui/material/styles/ThemeProvider"
+import theme from "./theme"
 
-import CssBaseline from '@mui/material/CssBaseline';
-import ThemeProvider from '@mui/material/styles/ThemeProvider';
-import theme from './theme'
-
-import CustomDialog from "./components/CustomDialog"
-import Loader from "./components/Loader"
-import SelectFile from "./components/SelectFile"
-
-import useFfmpeg from "./ffmpeg/useFfmpeg"
 import VideoSuite from "./pages/VideoSuite"
 
 function App() {
-	const [openDialog, setOpenDialog] = useState(false)
-	const { ready, load } = useFfmpeg()
-
-	const testEnv = process.env.NODE_ENV === "test"
-
-	useEffect(() => {
-		if (!testEnv) {
-			load()
-		}
-
-		setOpenDialog(!testEnv)
-	}, [])
-
 	return (
 		<ThemeProvider theme={theme}>
 			<CssBaseline />
 			<VideoSuite />
-			<CustomDialog
-				title={
-					ready
-						? "Please select a video file to continue"
-						: "Wait while the app is loaidng..."
-				}
-				openFromProps={openDialog}
-				maxWidth="sm"
-				fullWidth
-				hideCloseBtn
-				freeze
-			>
-				{!ready ? (
-					<Loader status={ready ? 100 : undefined} />
-				) : (
-					<SelectFile fileType="video" callback={() => setOpenDialog(false)} />
-				)}
-			</CustomDialog>
 		</ThemeProvider>
 	)
 }
--- src/pages/VideoSuite.tsx
+++ src/pages/VideoSuite.tsx
@@ -1,13 +1,50 @@
-import { FC } from "react"
+import { FC, useState, useEffect } from "react"
 
 import Container from "@mui/material/Container"
 import Box from "@mui/material/Box"
+import CustomDialog from "../components/CustomDialog"
+import Loader from "../components/Loader"
+import SelectFile from "../components/SelectFile"
+
+import useFfmpeg from "../ffmpeg/useFfmpeg"
 
 const VideoSuite: FC = () => {
+	const [openDialog, setOpenDialog] = useState(false)
+	const { ready, load } = useFfmpeg()
+
+	const testEnv = process.env.NODE_ENV === "test"
+
+	useEffect(() => {
+		if (!testEnv) {
+			load()
+		}
+
+		setOpenDialog(!testEnv)
+	}, [])
+
 	return (
 		<Container maxWidth="xl">
 			{/* <h1>Hello World!</h1> */}
 			<Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
+
+			<CustomDialog
+				title={
+					ready
+						? "Please select a video file to continue"
+						: "Wait while the app is loaidng..."
+				}
+				openFromProps={openDialog}
+				maxWidth="sm"
+				fullWidth
+				hideCloseBtn
+				freeze
+			>
+				{!ready ? (
+					<Loader status={ready ? 100 : undefined} />
+				) : (
+					<SelectFile fileType="video" callback={() => setOpenDialog(false)} />
+				)}
+			</CustomDialog>
 		</Container>
 	)
 }
