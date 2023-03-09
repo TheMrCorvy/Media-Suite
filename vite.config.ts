cc2172de20434bc0e6c3cf523d0115cc787f80af
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2023-03-09 12:13:45 +0000

    testing netlify integration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/Media-Suite/",
 	esbuild: {
 		jsxInject: "import React from 'react'",
 	},
