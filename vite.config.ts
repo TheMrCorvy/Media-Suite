420cbab6cc1a549ef996e06d11215ae9401c9ec6
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2023-03-09 08:31:19 +0000

    testing deployment on gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/Media-Suite/",
 	esbuild: {
 		jsxInject: "import React from 'react'",
 	},
